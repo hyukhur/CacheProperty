c94682bf60d68109602a8c911b79ca665b7152f0
Hyuk Hur <hyukhur@gmail.com>
2019-11-20 09:57:18 -0800

    Initial implements

--- Sources/CacheProperty/CacheProperty.swift
+++ Sources/CacheProperty/CacheProperty.swift
@@ -1,3 +1,77 @@
-struct CacheProperty {
-    var text = "Hello, World!"
+import Foundation
+
+protocol StorageProtocol {
+//    func object(forKey key: Any) -> Any?
+//    func setObject(_ obj: Any, forKey key: Any)
+//    func remove(forKey key: Any) -> Void
+}
+
+protocol Storage: StorageProtocol {
+    associatedtype K: Equatable
+    associatedtype V: Equatable
+    func object(forKey key: K) -> V?
+    func setObject(_ obj: V, forKey key: K)
+    func remove(forKey key: K) -> Void
+}
+
+struct NSCacheWrapper<KeyType: Equatable, ObjectType: Equatable>: Storage {
+    typealias K = KeyType
+    typealias V = ObjectType
+    
+    fileprivate class KeyWrapper: Equatable {
+        let value: KeyType
+        init(_ value: KeyType) {
+            self.value = value
+        }
+        static func == (lhs: NSCacheWrapper<KeyType, ObjectType>.KeyWrapper, rhs: NSCacheWrapper<KeyType, ObjectType>.KeyWrapper) -> Bool {
+            return lhs.value == rhs.value
+        }
+    }
+    fileprivate class ValueWrapper: Equatable {
+        let value: ObjectType
+        init(_ value: ObjectType) {
+            self.value = value
+        }
+        static func == (lhs: NSCacheWrapper<KeyType, ObjectType>.ValueWrapper, rhs: NSCacheWrapper<KeyType, ObjectType>.ValueWrapper) -> Bool {
+            return lhs.value == rhs.value
+        }
+    }
+    
+    fileprivate let cache = NSCache<KeyWrapper, ValueWrapper>()
+    
+    func object(forKey key: K) -> V? {
+        return self.cache.object(forKey: KeyWrapper(key))?.value
+    }
+    func setObject(_ obj: V, forKey key: K) {
+        self.cache.setObject(ValueWrapper(obj), forKey: KeyWrapper(key))
+    }
+    func remove(forKey key: K) {
+        self.cache.removeObject(forKey: KeyWrapper(key))
+    }
+}
+
+@propertyWrapper
+struct Cache<K: Equatable, V: Equatable> {
+    var storage: NSCacheWrapper<K, V>
+    var key: K
+    var missing: ((K) -> V)
+
+    init(key: K, storage: NSCacheWrapper<K, V> = NSCacheWrapper<K, V>(), missing: @escaping (K) -> V) {
+        self.key = key
+        self.storage = storage
+        self.missing = missing
+    }
+    
+    var wrappedValue: V {
+        if let value: V = self.storage.object(forKey: key) {
+            return value
+        }
+        let value = missing(key)
+        self.storage.setObject(value, forKey: key)
+        return value
+    }
+
+    func reset() {
+        self.storage.remove(forKey: key)
+    }
 }
--- Tests/CachePropertyTests/CachePropertyTests.swift
+++ Tests/CachePropertyTests/CachePropertyTests.swift
@@ -2,11 +2,38 @@ import XCTest
 @testable import CacheProperty
 
 final class CachePropertyTests: XCTestCase {
+    static var loadValue: (NSString) -> NSNumber = {
+        switch $0 {
+        case "key":
+            return 1
+        case "key2":
+            return 2
+        default:
+            return 1
+        }
+    }
+
+    @Cache<NSString, NSNumber>(key: "default", missing: loadValue) var cached: NSNumber
+//    @Cache<String, Int>(key: "default", storage: NSCacheWrapper<String, Int>(), missing: loadValue) var cached: Int
+
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CacheProperty().text, "Hello, World!")
+        XCTAssertEqual(cached, 1)
+    }
+    
+    func testReset() {
+        XCTAssertEqual(cached, 1)
+
+        _cached.reset()
+        _cached.missing = { _ in
+            return 10
+        }
+        XCTAssertEqual(cached, 10)
+    }
+    
+    func testKey() {
+        _cached.reset()
+        _cached.key = "key2"
+        XCTAssertEqual(cached, 2)
     }
 
     static var allTests = [
